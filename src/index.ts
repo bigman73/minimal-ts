4b5e7ee7ad5269d482e7fc2a0811d90ec36edd33
Yuval Naveh <bigman73@users.noreply.github.com>
2023-01-21 21:42:16 -0500

    refactor: index file to submodules

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,2 @@
-/**
- * Multiples two numbers.
- *
- * @param a -First number.
- * @param b - Second number.
- * @example const x = multiply(5,6)
- * @returns Multiplication result.
- */
-export const multiply = (a: number, b: number): number => {
-  return a * b
-}
-
-/**
- * Wraps input in array.
- *
- * @param input - Input.
- * @returns Wrapper array.
- */
-export const wrapInArray = <Type>(input: Type): Type[] => {
-  return [input]
-}
+export * from './mathOperations'
+export * from './wrappers'
---
+++ src/wrappers.ts
@@ -0,0 +1,9 @@
+/**
+ * Wraps input in array.
+ *
+ * @param input - Input.
+ * @returns Wrapper array.
+ */
+export const wrapInArray = <Type>(input: Type): Type[] => {
+  return [input]
+}
